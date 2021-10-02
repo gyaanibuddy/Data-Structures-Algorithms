3f2d0cc423c041fd15fb4c05de9d5330dbfe0fbb
Venkatesh-24 <venkateshsundar24@gmail.com>
2021-10-02 16:46:27 +0530

    Added 2 Sorting Algorithms and 1 Recursion Program

--- Recursion/NthFibonacci.java
+++ Recursion/NthFibonacci.java
@@ -6,7 +6,7 @@ public class NthFibonacci
         Scanner sc = new Scanner(System.in);
         System.out.println("Enter n : ");
         int n = sc.nextInt();
-        System.out.println("The " + n + "th Fibonacci number is = " + calcFibonacci(n));
+        System.out.println("The " + n + "th Fibonacci number is : " + calcFibonacci(n));
     }
 
     public static int calcFibonacci(int n)
