0d66eb38566f2e0bf222a8806433edeafb91d0d9
Venkatesh-24 <venkateshsundar24@gmail.com>
2021-10-02 16:19:49 +0530

    Added Finding Nth Fibonacci Number

---
+++ Recursion/NthFibonacci.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+public class NthFibonacci
+{
+    public static void main(String[] args)
+    {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter n : ");
+        int n = sc.nextInt();
+        System.out.println("The " + n + "th Fibonacci number is = " + calcFibonacci(n));
+    }
+
+    public static int calcFibonacci(int n)
+    {
+        if(n == 0 || n == 1)
+        {
+            return n;
+        }
+
+        return calcFibonacci(n-1) + calcFibonacci(n-2);
+    }
+}
